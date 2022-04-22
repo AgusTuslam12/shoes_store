d69f9f2c3f53d5318f2cc01b6bee445fc1d24447
AgusTuslam12 <agustuslam12@gmail.com>
2022-04-23 03:13:31 +0700

    first commit

---
+++ lib/theme.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+Color fontColor = HexColor("#081D43");
+Color whiteColor = HexColor("#FFFFFF");
+
+TextStyle textColor = GoogleFonts.inter(
+  color: fontColor,
+);
+
+const FontWeight bold = FontWeight.w700;
+const FontWeight semiBold = FontWeight.w600;
+const FontWeight medium = FontWeight.w500;
+const FontWeight reguler = FontWeight.w400;
+const FontWeight italic = FontWeight.w300;
